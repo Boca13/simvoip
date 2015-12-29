08b790601f744b82448be1766509bced8c455e97
David Barranco <david.ba94@gmail.com>
2015-12-29 12:38:20 +0100

    Idea principal sobre el escenario a montar: Añadidas sedes, conjunto de telefonos y sumidero en cada uno de ellos.
    Errores que tengo que solucionar:
    -Estoy instalando a los R entre sedes las app de los telefonos.
    -Tenemos que buscar algun algoritmo RoundRobin para uso de diferentes enlaces
    -Mirar todo's en el codigo para mas info

--- simvoip.cpp
+++ simvoip.cpp
@@ -17,18 +17,22 @@ NS_LOG_COMPONENT_DEFINE("Simvoip");
 // Punto 2D
 typedef std::pair<uint8_t, uint8_t> Punto;
 
+
 /*
-*	Recibe el número de enlaces y su velocidad y el objeto observador a utilizar
+*	Recibe el nï¿½mero de enlaces y su velocidad y el objeto observador a utilizar
 *	devuelve la QoS conseguida
 */
-double simular(uint8_t enlaces, DataRate velocidad, Observador *observador);
+double simular(uint8_t enlaces, DataRate velocidad, Observador *observador,uint32_t telef1,uint32_t telef2);
 
+
+using namespace ns3;
 
 int main()
 {
 	double qos_objetivo = 1.0;
+	//todo a los datarate tendras que meterle los Mbps o lo que sea
 	DataRate velocidades[10] = {DataRate(1), DataRate(2), DataRate(5), DataRate(10), DataRate(20), DataRate(30), DataRate(50), DataRate(100), DataRate(200), DataRate(300)};
-
+	Time::SetResolution(Time::US);
 
 
 	double qos = 0.0;
@@ -37,7 +41,7 @@ int main()
 	Observador * observador = new Observador();
 	std::map<Punto, double> historia;
 	std::map<Punto, double>::iterator iterador;
-	// Los resultados de las simulaciones se guardan en un map para el algoritmo de predicción
+	// Los resultados de las simulaciones se guardan en un map para el algoritmo de predicciï¿½n
 	while (qos < qos_objetivo)
 	{
 		qos = simular(enlaces, velocidades[velocidad], observador);
@@ -48,12 +52,83 @@ int main()
 
 	return 0;
 }
+//todo decidir que velocidades tendra la red interna de cada sede, por ahora supongo
+//velocidades de 100Mbps
+//todo repasar tipo de telef1, demasiado grande?
+//todo posible error: a los Routers les estoy instalando las apps que llevan los telefonos y les estoy dando direccionamiento
+//solucionar eso
 
-double simular(uint8_t enlaces, DataRate velocidad, Observador * observador)
+double simular(uint8_t enlaces, DataRate velocidad, Observador * observador,uint32_t telef1,uint32_t telef2 )
 {
 	// Preparar escenario
 
-	// Ejecutar simulación
+	NodeContainer Sedes;		//Creamos 2 sedes que vamos a interconectar
+	Sedes.Create(2);			//Son nodos p2p
+
+	//todo supongo que
+
+	//Creo telefonos de sede 1
+	NodeContainer telefonosSede1;
+	telefonosSede1.Add( Sedes.Get(0) );
+	telefonosSede1.Create(telef1);  //Creo el numero de telefonos que se le pase por parametros
+
+	//Creo telefonos de sede 2
+	NodeContainer telefonosSede2;
+	telefonosSede2.Add( Sedes.Get(1) );
+	telefonosSede2.Create(telef2);
+
+	//Instalo dispositivo en Routers entre sedes
+	PointToPointHelper pointToPoint;
+	NetDeviceContainer p2pDevices;
+	pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("1Gbps")); //todo supongo datos, ya se cambiara
+	pointToPoint.SetChannelAttribute ("Delay", StringValue ("0.1ms"));
+	p2pDevices = pointToPoint.Install (Sedes);
+
+	//Instalo dispositivo en telefonos Sede 1
+
+	PointToPointHelper pointToPointSede1;
+	NetDeviceContainer p2pDevicesSede1;
+	pointToPointSede1.SetDeviceAttribute ("DataRate", StringValue ("100Mbps")); //todo supongo datos, ya se cambiara
+	pointToPointSede1.SetChannelAttribute ("Delay", StringValue ("2ms"));
+	p2pDevicesSede1 = pointToPoint.Install (telefonosSede1);
+
+	//Instalo dispositivo en telefonos Sede 2
+
+	PointToPointHelper pointToPointSede2;
+	NetDeviceContainer p2pDevicesSede2;
+	pointToPointSede2.SetDeviceAttribute ("DataRate", StringValue ("100Mbps")); //todo supongo datos, ya se cambiara
+	pointToPointSede2.SetChannelAttribute ("Delay", StringValue ("2ms"));
+	p2pDevicesSede2 = pointToPoint.Install (telefonosSede2);
+
+	//Instalo pila TCP/IP en todos los nodos
+	 InternetStackHelper stack;
+	 stack.Install(telefonosSede1);	//todo tengo que instalar pila tcp/ip en Routers?
+	 stack.Install(telefonosSede2);
+
+	//Direccionamiento IP
+	 Ipv4AddressHelper address;
+	 Ipv4InterfaceContainer p2pInterfacesSede1;
+	 address.SetBase("10.1.1.0","255.255.0.0");
+	 p2pInterfacesSede1 = address.Assign(p2pDevicesSede1);
+	 Ipv4InterfaceContainer p2pInterfacesSede2;
+	 address.SetBase("10.1.2.0","255.255.0.0");
+	 p2pInterfacesSede2 = address.Assign(p2pDevicesSede2);
+
+	 Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+	 //Establezco un sumidero de paquetes en cada telefono
+	 //Utilizamos el puerto 9 para el sumidero
+	 uint16_t port = 9;
+	 PacketSinkHelper sink ("ns3::UdpSocketFactory", Address (InetSocketAddress (Ipv4Address::GetAny (), port)));
+	 ApplicationContainer sumidero = sink.Install(telefonosSede1);
+	 ApplicationContainer sumidero = sink.Install(telefonosSede2);
+
+	 //todo Crear aplicacion OnOff (mirar voip.cc y aÃ±adirlo a cada telefono)
+
+
+
+
+	// Ejecutar simulaciï¿½n
 
 
 	// Calcular calidad de servicio y devolverla
