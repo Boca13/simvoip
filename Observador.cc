6b9554f4d6b44043a9696f32e5aaed6f1e69cceb
David Barranco <david.ba94@gmail.com>
2016-01-28 20:51:33 +0100

    Arreglando fallos de alberto.
    Sigue dando un problema en la creacion de un enlace p2p, hay que tirar
    de debugger.

---
+++ Observador.cc
@@ -0,0 +1,103 @@
+#include "Observador.h"
+
+NS_LOG_COMPONENT_DEFINE ("Observador");
+Observador::Observador()
+{
+  m_enviados=0;
+  m_recibidos=0;
+  m_perdidos=0;
+  m_QoS=0;
+  m_usuarios=0;
+}
+
+
+Observador::~Observador()
+{
+}
+
+void
+Observador::PktEncolado(Ptr<const Packet>paquete)
+{
+   t_encolado=Simulator::Now();
+  uint64_t id = paquete->GetUid();
+ array[id]=t_encolado;
+}
+
+
+
+
+void
+Observador::PktEnviado(Ptr<const Packet>paquete)
+{
+  /*suponemos que el paquete está en la estructura*/
+    std::map<uint64_t,Time>::iterator aux=array.find(paquete->GetUid());
+     Time Taux =array[paquete->GetUid()]; 
+     Jitter.Update((Simulator::Now()-Taux).GetMilliSeconds());
+     m_enviados++;
+  
+}
+
+
+
+void 
+Observador::PktRecibido(Ptr<const Packet>paquete, const Address & dir)
+{
+  std::map<uint64_t,Time>::iterator aux=array.find(paquete->GetUid());
+  if (array.end() == aux)
+    {
+       NS_LOG_INFO("NO ESTA EN LA ESTRUCTURA");
+     }
+      else
+        {
+          NS_LOG_INFO("SI ESTA EN LA ESTRUCTURA");
+          Time Taux =array[paquete->GetUid()];
+          Retardo.Update((Simulator::Now()-Taux).GetMilliSeconds());
+          m_recibidos=m_recibidos+1;
+          array.erase(aux);
+        }
+}
+
+float
+Observador::QoSActual()
+{
+  
+   /*
+    modelo E 
+    QoS= (Ro − Is) − Id − Ie-eff + A 
+   jitter >= 100ms
+    retardo >= 150ms
+    % >=1
+  
+    */
+   m_perdidos=array.size();
+   /* debe ser menor a  un 0.01 o un 1%*/
+   m_porcentaje=((m_perdidos)/m_enviados)*100; //referente a paquetes perdidos
+  
+  
+    m_QoS=(m_porcentaje/PORCENTAJE + Jitter.Mean()/JITTER + Retardo.Mean()/RETARDO)/3;
+  return m_QoS; 
+}
+
+
+
+
+float 
+Observador::ActualizaJitter()
+{
+  /* el jitter debe se menor de 100 ms */
+  return Jitter.Mean();
+}
+
+float
+Observador::ActualizaRetardo()
+{
+  /* el retardo debe ser menor de 150ms */
+  return Retardo.Mean();
+}
+
+void
+Observador::Reset()
+{
+  Jitter.Reset();
+  Retardo.Reset();
+}
--- Observador.h
+++ Observador.h
@@ -2,6 +2,12 @@
 
 #include <ns3/packet.h>
 #include <ns3/average.h>
+#include "ns3/core-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/ipv4-global-routing-helper.h"
+
 #define JITTER 100 
 #define RETARDO 150
 #define PORCENTAJE  1
@@ -14,9 +20,9 @@ class Observador
 public:
 	Observador();
 	virtual ~Observador();
-	void PktEnviado(Ptr<Const Packet>paquete);
-	void PktRecibido(Ptr<Const Packet>paquete);
-	void PktEncolado(Ptr<Const Packet>paquete);
+	void PktEnviado(Ptr<const Packet>paquete);
+	void PktRecibido(Ptr<const Packet>paquete, const Address & dir);
+	void PktEncolado(Ptr<const Packet>paquete);
 	float ActualizaJitter(); /* función que actualiza el jitter cada vez que se envia*/
 	float ActualizaRetardo();/* función que actualiza el retardo */
 	float QoSActual(); /*Función que devuelvo el QoS alcanzado*/
@@ -24,14 +30,14 @@ public:
 	std::map<uint64_t, Time> array;
 private:
 	Time t_enviado; /* time para introducir en la estructura   (se deja de utilizar)*/
-	Time t_encolado /* tiempo con el que se encola un paquete en el buffer usado para introducir en la estructura*/
+	Time t_encolado; /* tiempo con el que se encola un paquete en el buffer usado para introducir en la estructura*/
 	float m_enviados;
 	float m_recibidos;
 	float m_perdidos;
-	float m_porcentaje /* variable auxiliar para el porcentaje de paquetes perdidos */
+	float m_porcentaje; /* variable auxiliar para el porcentaje de paquetes perdidos */
 	float m_QoS;   /* Qos que devolverá QoSActual*/
 	uint64_t m_usuarios; /* (Duda) para calcular usuarios totales hablando al mismo tiempo*/
 	Average<float>Jitter;  /* jitter del buffer */
-	Average<float>Retardo /* retardo de propagación*/
+	Average<float>Retardo; /* retardo de propagación*/
 };
 
