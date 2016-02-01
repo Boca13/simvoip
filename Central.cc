58bbdefdc6d87a76e61bd0e5483158eddd88c77c
Jesús Boca <jbl_13@hotmail.com>
2016-02-01 14:11:34 +0100

    repair

--- Central.cc
+++ Central.cc
@@ -16,12 +16,12 @@ Address Central::llamar(uint32_t llamante, Time duracion)
 {
 	ns3::UniformRandomVariable rnd;
 	// Se escoge aleatoriamente un número al que llamar
-	int llamado = rnd.GetValue(0, tel1+tel2);
+	uint32_t llamado = rnd.GetValue(0, tel1+tel2);
 	// Si el número no se encuentra en la lista, es que está libre
 	if (llamadas.find(llamado)==llamadas.end())
 	{
 		llamadas.insert(std::pair<uint32_t, uint32_t>(llamante, llamado));
-		Simulator::Schedule(duracion, this, Central::colgar, llamante);
+		Simulator::Schedule(duracion, this, colgar, llamante);
 		return telefonos[llamado];
 	}
 	else // Si se encontró, está ocupado y no se produce la llamada
@@ -37,7 +37,7 @@ void Central::colgar(uint32_t llamante)
 
 uint32_t Central::registrar(Address llamante)
 {
-	uint32_t indice = telefonos.count;
+	uint32_t indice = (uint32_t) telefonos.count;
 	telefonos.insert(std::pair<uint32_t, Address>(telefonos.count, llamante));
 	return indice;
 }
